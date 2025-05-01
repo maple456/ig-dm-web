89de6bfdda501e428c92f60d6a394ca768b66e4e
unknown <k91111699@yahoo.com.tw>
2025-05-01 20:14:33 +0800

    Fix server startup

--- ig_dm_web_api.py
+++ ig_dm_web_api.py
@@ -166,5 +166,7 @@ def handle_dm():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
 
